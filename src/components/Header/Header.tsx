2e4cd4e1d5da0f016a12542527b771066cd37da9
Sajjad Bayat <sajjad.bayat32@gmail.com>
2025-01-07 13:00:09 +0330

    extract header from main app to new component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
+const Titles = ["Fundamentals", "Core", "Better to know"];
+function getRandomTitle() {
+	const rnd = Math.floor(Math.random() * 3);
+	return Titles[rnd];
+}
+
+function Header() {
+	return (
+		<header>
+			<img src={reactImg} alt="Stylized atom" />
+			<h1>React Essentials</h1>
+			<p>
+				{getRandomTitle()} React concepts you will need for almost any app you
+				are going to build!
+			</p>
+		</header>
+	);
+}
+
+export default Header;
