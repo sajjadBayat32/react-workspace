ac6074b9a971e35d1854c1079726ab48a148f191
Sajjad Bayat <sajjad.bayat32@gmail.com>
2025-01-11 18:00:49 +0330

    feat: add stateful player component

--- src/projects/TicTacToe/TicTacToe.tsx
+++ src/projects/TicTacToe/TicTacToe.tsx
@@ -1,6 +1,19 @@
 import Header from "./components/layout/Header";
+import Player from "./components/Player";
 import "./TicTacToe.scss";
 
 export default function TicTacToe() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<main>
+				<div id="game-container">
+					<ol id="players">
+						<Player initialName="Player 1" symbol="X" />
+						<Player initialName="Player 2" symbol="O" />
+					</ol>
+				</div>
+			</main>
+		</>
+	);
 }
---
+++ src/projects/TicTacToe/components/Player.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, useState } from "react";
+
+export default function Player({ initialName, symbol }: ComponentProps) {
+	const [playerName, setPlayerName] = useState(initialName);
+	const [isEditing, setIsEditing] = useState<boolean>(false);
+
+	function handleEditClick() {
+		setIsEditing((editing) => !editing);
+	}
+
+	function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+		setPlayerName(event.target.value);
+	}
+
+	let playerNameDom = <span className="player-name">{playerName}</span>;
+	if (isEditing) {
+		playerNameDom = (
+			<input
+				type="text"
+				required
+				value={playerName}
+				onChange={handleInputChange}
+			/>
+		);
+	}
+
+	return (
+		<li>
+			<span className="player">
+				{playerNameDom}
+				<span className="player-symbol">{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+		</li>
+	);
+}
+
+interface ComponentProps {
+	initialName: string;
+	symbol: string;
+}
