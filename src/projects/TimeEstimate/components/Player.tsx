d1c0662c9cfc24545509721fbc44c2137e9a6599
Sajjad Bayat <sajjad.bayat32@gmail.com>
2025-02-22 14:43:10 +0330

    feat: use useRef hook to read input value

--- src/projects/TimeEstimate/components/Player.tsx
+++ src/projects/TimeEstimate/components/Player.tsx
@@ -1,10 +1,22 @@
+import { useRef, useState } from "react";
+
 function Player() {
+	const playerInput = useRef<HTMLInputElement>(null);
+	const [playerName, setPlayerName] = useState("");
+
+	function handleClick() {
+		const { current } = playerInput;
+		if (current) {
+			setPlayerName(current.value);
+		}
+	}
+
 	return (
 		<section id="player">
-			<h2>Welcome unknown entity</h2>
+			<h2>Welcome {playerName !== "" ? playerName : "unknown entity"}</h2>
 			<p>
-				<input type="text" />
-				<button>Set Name</button>
+				<input type="text" ref={playerInput} />
+				<button onClick={handleClick}>Set Name</button>
 			</p>
 		</section>
 	);
