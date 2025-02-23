d66d01ed37d1e336950fd07d27a978dd0f477146
sajjadBayat32 <sajjad.bayat32@gmail.com>
2025-02-23 20:47:30 +0330

    feat: create modal using Portal

--- src/projects/TimeEstimate/components/ResultModal.tsx
+++ src/projects/TimeEstimate/components/ResultModal.tsx
@@ -1,45 +1,47 @@
 import { RefObject, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 function ResultModal({
-	ref,
-	targetTime,
-	timeRemaining,
-	onClose,
+  ref,
+  targetTime,
+  timeRemaining,
+  onClose,
 }: ComponentProps) {
-	const dialog = useRef<HTMLDialogElement>(null);
-	const userLost = timeRemaining <= 0;
-	const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
-	const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+  const dialog = useRef<HTMLDialogElement>(null);
+  const userLost = timeRemaining <= 0;
+  const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
 
-	useImperativeHandle(ref, () => {
-		return {
-			open: () => dialog.current?.showModal(),
-		};
-	});
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => dialog.current?.showModal(),
+    };
+  });
 
-	return (
-		<dialog ref={dialog} className="result-modal">
-			{userLost && <h2>You lost</h2>}
-			{!userLost && <h2>Your Score: {score}</h2>}
-			<p>
-				The target time was <strong>{targetTime} seconds.</strong>
-			</p>
-			<p>
-				You stopped the timer with{" "}
-				<strong>{formattedRemainingTime} seconds left.</strong>
-			</p>
-			<form method="dialog" onSubmit={onClose}>
-				<button>Close</button>
-			</form>
-		</dialog>
-	);
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onClose}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")!
+  );
 }
 
 interface ComponentProps {
-	ref: RefObject<{ open: () => void }>;
-	targetTime: number;
-	timeRemaining: number;
-	onClose: () => void;
+  ref: RefObject<{ open: () => void }>;
+  targetTime: number;
+  timeRemaining: number;
+  onClose: () => void;
 }
 
 export default ResultModal;
