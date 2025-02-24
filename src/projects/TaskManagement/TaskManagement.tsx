dade0752fca314203148c154a68523b65ed16fff
Sajjad Bayat <sajjad.bayat32@gmail.com>
2025-02-24 11:13:41 +0330

    feat: display projects in sidebar

--- src/projects/TaskManagement/TaskManagement.tsx
+++ src/projects/TaskManagement/TaskManagement.tsx
@@ -5,52 +5,53 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function TaskManagement() {
-  const [projectsState, setProjectsState] = useState<State>({
-    selectedProjectId: undefined,
-    projects: [],
-  });
+	const [projectsState, setProjectsState] = useState<State>({
+		selectedProjectId: undefined,
+		projects: [],
+	});
 
-  console.log(projectsState);
+	function handleStartWithNewProject() {
+		setProjectsState((prev) => ({
+			...prev,
+			selectedProjectId: null,
+		}));
+	}
 
-  function handleStartWithNewProject() {
-    setProjectsState((prev) => ({
-      ...prev,
-      selectedProjectId: null,
-    }));
-  }
+	function handleAddProject(project: Omit<Project, "id" | "tasks">) {
+		const newProject: Project = {
+			...project,
+			id: Math.ceil(Math.random() * 1000),
+			tasks: [],
+		};
+		setProjectsState((prev) => ({
+			selectedProjectId: undefined,
+			projects: [...prev.projects, newProject],
+		}));
+	}
 
-  function handleAddProject(project: Omit<Project, "id" | "tasks">) {
-    const newProject: Project = {
-      ...project,
-      id: Math.ceil(Math.random() * 1000),
-      tasks: [],
-    };
-    setProjectsState((prev) => ({
-      ...prev,
-      projects: [...prev.projects, newProject],
-    }));
-  }
+	let pageContent;
+	if (projectsState.selectedProjectId === undefined) {
+		pageContent = (
+			<NoProjectSelected onStartWithNewProject={handleStartWithNewProject} />
+		);
+	} else if (projectsState.selectedProjectId === null) {
+		pageContent = <NewProject onAddProject={handleAddProject} />;
+	}
 
-  let pageContent;
-  if (projectsState.selectedProjectId === undefined) {
-    pageContent = (
-      <NoProjectSelected onStartWithNewProject={handleStartWithNewProject} />
-    );
-  } else if (projectsState.selectedProjectId === null) {
-    pageContent = <NewProject onAddProject={handleAddProject} />;
-  }
-
-  return (
-    <main className="h-screen py-8 flex gap-8">
-      <ProjectSidebar onStartWithNewProject={handleStartWithNewProject} />
-      {pageContent}
-    </main>
-  );
+	return (
+		<main className="h-screen py-8 flex gap-8">
+			<ProjectSidebar
+				projects={projectsState.projects}
+				onStartWithNewProject={handleStartWithNewProject}
+			/>
+			{pageContent}
+		</main>
+	);
 }
 
 interface State {
-  selectedProjectId: number | null | undefined;
-  projects: Project[];
+	selectedProjectId: number | null | undefined;
+	projects: Project[];
 }
 
 export default TaskManagement;
--- src/projects/TaskManagement/components/ProjectSidebar.tsx
+++ src/projects/TaskManagement/components/ProjectSidebar.tsx
@@ -1,21 +1,31 @@
+import { Project } from "../types/Project";
 import Button from "./Button";
 
-function ProjectSidebar({ onStartWithNewProject }: ComponentsProps) {
-  return (
-    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-e-xl">
-      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
-        Your Projects
-      </h2>
-      <div>
-        <Button onClick={onStartWithNewProject}>+ Add Project</Button>
-      </div>
-      <ul></ul>
-    </aside>
-  );
+function ProjectSidebar({ projects, onStartWithNewProject }: ComponentsProps) {
+	return (
+		<aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-e-xl">
+			<h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+				Your Projects
+			</h2>
+			<div>
+				<Button onClick={onStartWithNewProject}>+ Add Project</Button>
+			</div>
+			<ul className="mt-8">
+				{projects.map((p) => (
+					<li key={p.id} className="">
+						<button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+							{p.title}
+						</button>
+					</li>
+				))}
+			</ul>
+		</aside>
+	);
 }
 
 interface ComponentsProps {
-  onStartWithNewProject: () => void;
+	projects: Project[];
+	onStartWithNewProject: () => void;
 }
 
 export default ProjectSidebar;
