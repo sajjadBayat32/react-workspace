051879548a9dfb62496be44ce330331af8c5a402
Sajjad Bayat <sajjad.bayat32@gmail.com>
2025-03-01 12:26:43 +0330

    feat: add useEffect to fix infinite loop issue when sorting places based on location

--- src/projects/PlacePicker/PlacePicker.tsx
+++ src/projects/PlacePicker/PlacePicker.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./PlacePicker.scss";
 import { AVAILABLE_PLACES } from "./data";
 import Modal from "./components/Modal";
@@ -6,70 +6,84 @@ import DeleteConfirmation from "./components/DeleteConfirmation";
 import Places from "./components/Places";
 import { Place } from "./types/Place.type";
 import logoImg from "../../assets/place/logo.png";
+import { sortPlacesByDistance } from "./loc";
 
 function PlacePicker() {
-  const modal = useRef<{ open: () => void; close: () => void }>(null);
-  const selectedPlace = useRef<string>("");
-  const [pickedPlaces, setPickedPlaces] = useState<Place[]>([]);
+	const modal = useRef<{ open: () => void; close: () => void }>(null);
+	const selectedPlace = useRef<string>("");
+	const [availablePlaces, setAvailablePlaces] = useState<Place[]>([]);
+	const [pickedPlaces, setPickedPlaces] = useState<Place[]>([]);
 
-  function handleStartRemovePlace(id: string) {
-    modal.current?.open();
-    selectedPlace.current = id;
-  }
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			const sortedPlaces = sortPlacesByDistance(
+				AVAILABLE_PLACES,
+				position.coords.latitude,
+				position.coords.longitude
+			);
+			setAvailablePlaces(sortedPlaces);
+		});
+	}, []);
 
-  function handleStopRemovePlace() {
-    modal.current?.close();
-  }
+	function handleStartRemovePlace(id: string) {
+		modal.current?.open();
+		selectedPlace.current = id;
+	}
 
-  function handleSelectPlace(id: string) {
-    setPickedPlaces((prevPickedPlaces) => {
-      if (prevPickedPlaces.some((place) => place.id === id)) {
-        return prevPickedPlaces;
-      }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
-      return [place, ...prevPickedPlaces];
-    });
-  }
+	function handleStopRemovePlace() {
+		modal.current?.close();
+	}
 
-  function handleRemovePlace() {
-    setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
-    );
-    modal.current?.close();
-  }
+	function handleSelectPlace(id: string) {
+		setPickedPlaces((prevPickedPlaces) => {
+			if (prevPickedPlaces.some((place) => place.id === id)) {
+				return prevPickedPlaces;
+			}
+			const place = AVAILABLE_PLACES.find((place) => place.id === id);
+			return [place, ...prevPickedPlaces];
+		});
+	}
 
-  return (
-    <>
-      <Modal ref={modal}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
-        />
-      </Modal>
+	function handleRemovePlace() {
+		setPickedPlaces((prevPickedPlaces) =>
+			prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+		);
+		modal.current?.close();
+	}
 
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText={"Select the places you would like to visit below."}
-          places={pickedPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
-        <Places
-          title="Available Places"
-          places={AVAILABLE_PLACES}
-          onSelectPlace={handleSelectPlace}
-        />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Modal ref={modal}>
+				<DeleteConfirmation
+					onCancel={handleStopRemovePlace}
+					onConfirm={handleRemovePlace}
+				/>
+			</Modal>
+
+			<header>
+				<img src={logoImg} alt="Stylized globe" />
+				<h1>PlacePicker</h1>
+				<p>
+					Create your personal collection of places you would like to visit or
+					you have visited.
+				</p>
+			</header>
+			<main>
+				<Places
+					title="I'd like to visit ..."
+					fallbackText={"Select the places you would like to visit below."}
+					places={pickedPlaces}
+					onSelectPlace={handleStartRemovePlace}
+				/>
+				<Places
+					title="Available Places"
+					places={availablePlaces}
+					fallbackText="Sorting the places..."
+					onSelectPlace={handleSelectPlace}
+				/>
+			</main>
+		</>
+	);
 }
 
 export default PlacePicker;
